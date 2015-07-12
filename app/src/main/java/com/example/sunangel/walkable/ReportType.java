862a05c0764b44fb303979c14d6a71fd722cd72d
Ian Macalinao <me@ian.pw>
2015-07-11 17:13:22 -0700

    Add list of report types

--- app/src/main/java/com/example/sunangel/walkable/ReportType.java
+++ app/src/main/java/com/example/sunangel/walkable/ReportType.java
@@ -1,12 +1,26 @@
 package com.example.sunangel.walkable;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ian on 7/11/15.
  */
 public class ReportType {
+    static final List<ReportType> reportTypes = new ArrayList<>();
+
+    static {
+        reportTypes.add(new ReportType(R.drawable.common_signin_btn_icon_dark, "Police", 1.0));
+        reportTypes.add(new ReportType(R.drawable.common_signin_btn_icon_dark, "Suspicious Person", 1.0));
+        reportTypes.add(new ReportType(R.drawable.common_signin_btn_icon_dark, "No Lighting", 1.0));
+        reportTypes.add(new ReportType(R.drawable.common_signin_btn_icon_dark, "Busy Area", 1.0));
+    }
+
     private String name;
     private int icon;
-    
+
     // How much this report influences the navigation, [0, 1]
     private double factor = 1.0;
 
@@ -26,5 +40,9 @@ public class ReportType {
 
     public double getFactor() {
         return factor;
+    }
+
+    public Report createReport(LatLng location) {
+        return new Report(this, location);
     }
 }
